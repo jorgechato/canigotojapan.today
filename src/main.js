6e6acd90cbf3e8edb524b9d59c2cb6265b4520f1
jorgechato <hack@jorgechato.com>
2021-03-22 17:40:15 +0100

    ADD Bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueAnalytics from "vue-analytics";
+import 'bootstrap-4-grid/css/grid.min.css'
 
 Vue.config.productionTip = false
 
